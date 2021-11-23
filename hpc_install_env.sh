a43f5eb8ec4dbb7c93a3e07e59406a7745270d0e
tvhahn <tvhahn@gmail.com>
2021-11-22 19:28:48 -0500

    pip install -e .

--- hpc_install_env.sh
+++ hpc_install_env.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-cd
 module load python/3.8
 virtualenv ~/featstore
 source ~/featstore/bin/activate
@@ -7,6 +6,7 @@ pip install --no-index --upgrade pip
 pip install --no-index pandas scipy scikit_learn matplotlib seaborn
 pip install --no-index jupyterlab click h5py
 pip install tsfresh
+pip install -e .
 
 # create bash script for opening jupyter notebooks https://stackoverflow.com/a/4879146/9214620
 cat << EOF >$VIRTUAL_ENV/bin/notebook.sh
