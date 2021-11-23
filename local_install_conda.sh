b84ed6c006a6ea493a81c84d48bd8e64413ac45d
tvhahn <tvhahn@gmail.com>
2021-11-23 09:34:49 -0700

    simple tsfresh example

--- local_install_conda.sh
+++ local_install_conda.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-conda install -n base -c conda-forge python=3.8 mamba
+conda install -n base -c conda-forge mamba
 mamba env create -f envfeatstore.yml
 eval "$(conda shell.bash hook)"
 conda activate featstore
