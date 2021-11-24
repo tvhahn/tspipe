c26990437e060f7729e41a96676625dda7a8bf48
tvhahn <tvhahn@gmail.com>
2021-11-24 09:14:25 -0700

    start adding testing

---
+++ tests/test_milling.py
@@ -0,0 +1,14 @@
+import unittest
+from pathlib import Path
+from src.data.data_prep_utils import MillingDataPrep
+
+
+
+
+if __name__ == '__main__':
+
+    
+    project_dir = Path(__file__).resolve().parents[1]
+
+    unittest.main()
+
