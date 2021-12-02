e2956fd9e2247e30c888717cbde08ed634a396e9
tvhahn <tvhahn@gmail.com>
2021-12-02 14:05:40 -0800

    first commit for slurm scripts

---
+++ src/data/make_raw_data_hpc.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --time=00:10:00 # 10 minutes
+#SBATCH --mem=4G
+#SBATCH --account=rrg-mechefsk
+#SBATCH --mail-type=ALL               # Type of email notification- BEGIN,END,F$
+#SBATCH --mail-user=18tcvh@queensu.ca   # Email to which notifications will be $
+
+module load python/3.8
+
+PROJECT_DIR=$1
+
+SCRATCH_DATA_DIR = ~/scratch/feat-store/data
+
+source ~/featstore/bin/activate
+
+python $PROJECT_DIR/src/data/make_dataset.py --path_data_folder $SCRATCH_DATA_DIR
---
+++ src/features/build_features_hpc.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --time=00:30:00 # 30 min
+#SBATCH --array=1-10
+#SBATCH --cpus-per-task=4
+#SBATCH --account=rrg-mechefsk
+#SBATCH --mem=8G
+#SBATCH --mail-type=ALL               # Type of email notification- BEGIN,END,F$
+#SBATCH --mail-user=18tcvh@queensu.ca   # Email to which notifications will be $
+## How to use arrays: https://docs.computecanada.ca/wiki/Job_arrays
+
+echo "Starting task $SLURM_ARRAY_TASK_ID"
+
+module load python/3.8
+
+PROJECT_DIR=$1
+
+SCRATCH_DATA_DIR = ~/scratch/feat-store/data
+
+source ~/featstore/bin/activate
+
+python $PROJECT_DIR/src/features/build_features.py --path_data_folder $SCRATCH_DATA_DIR --n_chunks 10 --chunk_index $SLURM_ARRAY_TASK_ID
