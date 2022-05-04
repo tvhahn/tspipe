4845ec22f7d0a176c2474db036305762e54555f1
tvhahn <tvhahn@gmail.com>
2022-05-04 00:17:35 -0400

    update

--- src/models/train_hpc.sh
+++ src/models/train_hpc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --time=00:05:00 # 30 min
-#SBATCH --array=1-3
+#SBATCH --array=1-6
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=4G
 #SBATCH --account=rrg-mechefsk
@@ -16,5 +16,5 @@ SCRATCH_DATA_DIR=~/scratch/feat-store/data
 module load python/3.8
 source ~/featstore/bin/activate
 
-python $PROJECT_DIR/src/models/train.py --save_dir_name interim_results_$NOW_TIME --path_data_dir $SCRATCH_DATA_DIR --rand_search_iter 20 --feat_selection
+python $PROJECT_DIR/src/models/train.py --save_dir_name interim_results_$NOW_TIME --path_data_dir $SCRATCH_DATA_DIR --rand_search_iter 300 --feat_selection
 
