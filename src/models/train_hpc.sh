7c7445d269dd7479d565b043f3602a4f7a14a934
tvhahn <tvhahn@gmail.com>
2022-05-06 15:50:20 -0400

    update

--- src/models/train_hpc.sh
+++ src/models/train_hpc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --time=00:3:00 # 30 min
-#SBATCH --array=1-10
+#SBATCH --time=00:5:00 # 30 min
+#SBATCH --array=1-20
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=4G
 #SBATCH --account=rrg-mechefsk
