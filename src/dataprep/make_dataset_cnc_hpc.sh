9047e5e97e9478d35f7433d081e27f9dccda1c33
tvhahn <tvhahn@gmail.com>
2022-06-08 22:39:25 -0400

    update

--- src/dataprep/make_dataset_cnc_hpc.sh
+++ src/dataprep/make_dataset_cnc_hpc.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --time=00:15:00 # 10 minutes
+#SBATCH --time=01:15:00 # 10 minutes
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=8
-#SBATCH --mem=16G
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=64G
 #SBATCH --account=rrg-mechefsk
 #SBATCH --mail-type=ALL               # Type of email notification- BEGIN,END,F$
 #SBATCH --mail-user=18tcvh@queensu.ca   # Email to which notifications will be $
@@ -15,4 +15,4 @@ SCRATCH_DATA_DIR=~/scratch/feat-store/data
 
 source ~/featstore/bin/activate
 
-python $PROJECT_DIR/src/dataprep/make_dataset_cnc.py -p $PROJECT_DIR --path_data_dir $SCRATCH_DATA_DIR --split_dir_name data_splits --save_dir_name data_raw_processed --n_cores 8
+python $PROJECT_DIR/src/dataprep/make_dataset_cnc.py -p $PROJECT_DIR --path_data_dir $SCRATCH_DATA_DIR --split_dir_name data_splits --save_dir_name data_raw_processed --n_cores 32
