cf290d1bc1186b8a82e9bb5d7394fc59c9242743
tvhahn <tvhahn@gmail.com>
2022-07-19 09:49:10 -0400

    update

--- src/models/compile_cnc_hpc.sh
+++ src/models/compile_cnc_hpc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --time=00:05:00 # 10 minutes
+#SBATCH --time=00:10:00 # 10 minutes
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=8
 #SBATCH --mem=4G
 #SBATCH --account=rrg-mechefsk
 #SBATCH --mail-type=ALL               # Type of email notification- BEGIN,END,F$
@@ -16,7 +16,7 @@ source ~/featstore/bin/activate
 
 python $PROJECT_DIR/src/models/compile.py \
     -p $PROJECT_DIR \
-    --n_cores 4 \
+    --n_cores 8 \
     --interim_dir_name interim_results_cnc \
     --final_dir_name final_results_cnc
 
