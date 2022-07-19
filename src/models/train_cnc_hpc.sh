1862dcbb1a5238ba08c35fb06c00e7baae18dd5e
tvhahn <tvhahn@gmail.com>
2022-07-18 18:46:38 -0700

    Update with cnc train script

---
+++ src/models/train_cnc_hpc.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --time=00:10:00 # 30 min
+#SBATCH --array=1-2
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=8G
+#SBATCH --account=rrg-mechefsk
+#SBATCH --mail-type=ALL               # Type of email notification- BEGIN,END,F$
+#SBATCH --mail-user=18tcvh@queensu.ca   # Email to which notifications will be $
+
+PROJECT_DIR=$1
+
+NOW_TIME=$2
+
+SCRATCH_DATA_DIR=~/scratch/feat-store/data
+
+module load python/3.8
+source ~/featstore/bin/activate
+
+python $PROJECT_DIR/src/models/train.py \
+    --path_data_dir $SCRATCH_DATA_DIR \
+    --dataset cnc \
+    --save_dir_name interim_results_cnc \
+    --processed_dir_name cnc_features_custom_1 \
+    --feat_file_name cnc_features_54_custom_1.csv \
+    --rand_search_iter 300 \
+    --feat_selection
+
+
