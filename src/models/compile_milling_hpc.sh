39fa071e7866296b8a67f68a560e93138d6c0724
tvhahn <tvhahn@gmail.com>
2022-07-22 10:24:52 -0700

    Create script for compiling milling on hpc

--- src/models/compile_milling_hpc.sh
+++ src/models/compile_milling_hpc.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --time=00:10:00 # 10 minutes
+#SBATCH --time=00:05:00 # 10 minutes
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=6
-#SBATCH --mem=8G
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=4G
 #SBATCH --account=rrg-mechefsk
 #SBATCH --mail-type=ALL               # Type of email notification- BEGIN,END,F$
 #SBATCH --mail-user=18tcvh@queensu.ca   # Email to which notifications will be $
@@ -14,5 +14,9 @@ SCRATCH_DATA_DIR=~/scratch/feat-store/data
 module load python/3.8
 source ~/featstore/bin/activate
 
-python $PROJECT_DIR/src/models/compile.py -p $PROJECT_DIR --n_cores 6 --interim_dir_name interim_results_milling --final_dir_name final_results_milling
+python $PROJECT_DIR/src/models/compile.py \
+    -p $PROJECT_DIR \
+    --n_cores 8 \
+    --interim_dir_name interim_results_milling \
+    --final_dir_name final_results_milling
 
